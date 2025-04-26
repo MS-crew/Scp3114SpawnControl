2d24d01c6b667296b4475d4cea139fba9f1b3b4b
Mustafa SAVAŞ <mustafavesavas1@gmail.com>
2025-04-26 19:42:28 +0300

    Clean

---
+++ Config.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace Scp3114SpawnControl
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public bool Debug { get; set; } = false;
+
+        [Description("Chance for SCP-3114 to spawn (in percentage).")]
+        public int Chance { get; set; } = 50;
+
+        [Description("Minimum number of humans required for SCP-3114 to spawn.")]
+        public int MinimumHuman { get; set; } = 5;
+
+        [Description("Whether SCP-3114 will be selected from SCPs or humans. (True = SCPs / False = Humans)")]
+        public bool SelectFromScps { get; set; } = true;
+    }
+}
