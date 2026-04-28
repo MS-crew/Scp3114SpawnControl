4d3cc0bc6863caaf2fb83c27dd566d9acec38708
Mustafa SAVAŞ <mustafavesavas1@gmail.com>
2026-04-29 01:36:55 +0300

    more clean config

--- Models/SpawnPoint.cs
+++ Models/SpawnPoint.cs
@@ -9,10 +9,9 @@ namespace Scp3114SpawnControl.Models
 {
     public class SpawnPoint
     {
-        public float Chance { get; set; }
-
-        [Description("Name of the spawn point (for organization purposes only).")]
         public string Name { get; set; }
+
+        public float Chance { get; set; }
 
         [Description("The room type where SCP-3114 and the ragdolls will spawn.")]
         public RoomType Room { get; set; }
@@ -20,7 +19,7 @@ namespace Scp3114SpawnControl.Models
         [Description("Position offset relative to the room (or world coordinates if no room is found).")]
         public Vector3 Position { get; set; }
 
-        [Description("Rotation offset relative to the room (X, Y, Z).")]
+        [Description("Eular Rotation offset relative to the room.")]
         public Vector3 Rotation { get; set; }
 
         [Description("List of custom ragdolls to spawn around SCP-3114 at this location.")]
